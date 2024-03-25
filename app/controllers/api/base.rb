5a3cb14b3197de2bf238d6fbbc5276d08487b78d
A. H. M. Zobyer <ahm.zobyer@welldev.io>
2024-03-25 13:50:54 +0600

    feat: create blog

--- app/controllers/api/base.rb
+++ app/controllers/api/base.rb
@@ -1,5 +1,11 @@
 module API
   class Base < Grape::API
     mount API::V1::Base
+
+    helpers do
+      def permitted_params
+        declared(params, include_missing: false)
+      end
+    end
   end
 end
--- app/controllers/api/v1/resources/blog_posts.rb
+++ app/controllers/api/v1/resources/blog_posts.rb
@@ -3,16 +3,38 @@ module API
     module Resources
       class BlogPosts < Grape::API
         resources :blogs do
-          helpers do
-            def blog
-              @blog = BlogPost.first
-            end
-          end
-
           desc 'get all blogs'
           get do
             blogs = BlogPost.all
             present blogs, with: Entities::BlogPost
+          end
+
+          route_param :blog_id do
+            helpers do
+              def blog
+                @blog ||= BlogPost.find(params[:blog_id])
+              end
+            end
+
+            desc 'get blog details'
+            get do
+              present blog, with: Entities::BlogPost
+            end
+          end
+        end
+
+        resource :blog do 
+          desc 'create a blog'
+          params do
+            requires :title, type: String 
+            requires :body, type: String 
+          end
+
+          post do 
+            blog = BlogPost.new(**params, user: User.first)
+            
+            blog.save
+            present blog, with: Entities::BlogPost
           end
         end
       end
